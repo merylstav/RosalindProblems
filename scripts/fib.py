21f276d726e358b313d319019886a7a4de0d2be0
merylstav <merylstav@gmail.com>
2018-05-30 17:23:18 -0400

    Adding directions

--- scripts/fib.py
+++ scripts/fib.py
@@ -1,10 +1,14 @@
-n = 31
-k = 3
-f = {}
-f[0] = f[1] = 1
+print("Directions:")
+print("Given positive integers n <= 40 and k <= 5, return the total number of rabbit pairs that will be present after n months, if we begin with 1 pair and in each generation, every pair of reproduction-age rabbits produces a litter of k rabbit pairs (instead of only 1 pair).\n\n")
+f = open("../files/rosalind_fib.txt")
+n, k = map(int, f.readline().strip().split())
+print("n = %i\nk = %i" % (n, k))
+F = {}
+F[0] = F[1] = 1
 for i in range(2, n):
-	f[i] = f[i-1] + f[i-2]*k
-print(f[n-1])
+	F[i] = F[i-1] + F[i-2]*k
+print(F[n-1])
+# The option below returns the same output
 a = b = 1
 for i in range(2, n):
 	a, b = b, k*a + b
